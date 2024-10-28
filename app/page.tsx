29554f925d53378c9417c45c5383e70f6fafa975
aimerthyr <aimerthy@foxmail.com>
2024-10-28 17:34:17 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -21,12 +23,23 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span className={lusitana.className}>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex items-center flex-col justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+
+          <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+          />
+
+          <Image src="/hero-mobile.png" className='block md:hidden' width={560} height={620} alt=""></Image>
         </div>
+
       </div>
     </main>
   );
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'], display: "swap",adjustFontFallback: false });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '700'})
